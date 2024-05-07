cf73306d91defde0494037aaedd41b57a163e0f0
vijaypk <vijaypallot@gmail.com>
2024-05-07 14:50:05 +0530

    event section

--- neyndraglobal/src/app/home/event-carousel/event-carousel.component.ts
+++ neyndraglobal/src/app/home/event-carousel/event-carousel.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-event-carousel',
   templateUrl: './event-carousel.component.html',
   styleUrl: './event-carousel.component.scss'
 })
-export class EventCarouselComponent {
+export class EventCarouselComponent implements OnInit{
 
+  events: any[] = [];
+
+  constructor(){
+    this.events = [
+      {
+        img: 'assets/a.jpg',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        title: 'Title'
+      },
+      {
+        img: 'assets/b.jpg',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        title: 'Title'
+      },
+      {
+        img: 'assets/c.jpg',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        title: 'Title'
+      }
+    ]
+  }
+
+  ngOnInit(): void {
+      
+  }
 }
